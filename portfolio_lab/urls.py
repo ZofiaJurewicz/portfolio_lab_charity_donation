6b8c442ff8e6e3045f815095b7ef225ef3d33e1a
ZofiaJurewicz <zjurewicz@st.swps.edu.pl>
2024-03-13 01:06:32 +0100

    Preparation of templates

--- portfolio_lab/urls.py
+++ portfolio_lab/urls.py
@@ -17,6 +17,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from charity_donation_app.views import AddDonationView, LandingPageView, LoginView, RegisterView
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', LandingPageView.as_view(), name='landing_page'),
+    path('add_donation/', AddDonationView.as_view(), name='add_donation'),
+    path('login/', LoginView.as_view(), name='login'),
+    path('register/', RegisterView.as_view(), name='register'),
 ]
