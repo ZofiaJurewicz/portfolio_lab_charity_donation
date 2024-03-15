6db091139347ca92daa9ef03d16b0b0b7d8211dc
ZofiaJurewicz <zjurewicz@st.swps.edu.pl>
2024-03-15 01:01:41 +0100

    Registration view - form management

--- charity_donation_app/views.py
+++ charity_donation_app/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
 from django.views import View
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
 
 from charity_donation_app.models import Donation, Institution
 
@@ -34,3 +35,27 @@ class LoginView(View):
 class RegisterView(View):
     def get(self, request):
         return render(request, 'register.html', {'page_type': 'register'})
+
+    def post(self, request):
+        name = request.POST.get('name')
+        surname = request.POST.get('surname')
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+        password2 = request.POST.get('password2')
+
+        if not (name and surname and email and password and password2):
+            return redirect('register')
+
+        if password != password2:
+            return redirect('register')
+
+        if User.objects.filter(email=email).exists():
+            return redirect('register')
+
+        user = User.objects.create_user(username=email, email=email, password=password)
+        user.first_name = name
+        user.last_name = surname
+        user.save()
+
+        return redirect('login')
+
